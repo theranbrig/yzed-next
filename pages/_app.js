d670a07c0ea128308eb4fde1ad78dff4c9f75b54
Theran <theran.brigowatz@gmail.com>
2020-05-26 16:16:20 +0900

    basic meta - photos wip

--- pages/_app.js
+++ pages/_app.js
@@ -8,13 +8,38 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* <meta
+          property='og:image'
+          content='https://res.cloudinary.com/dq7uyauun/image/upload/q_auto/v1551968239/IMG_1880.jpg'
+        /> */}
+
+        {/* <meta
+          property='twitter:image'
+          content='https://res.cloudinary.com/dq7uyauun/image/upload/q_auto/v1551968239/IMG_1880.jpg'></meta> */}
+        {/*
+        <link rel='icon' type='image/x-icon' href='../static/favicon.ico' prefetch />
+        <link rel='apple-touch-icon' sizes='180x180' href='../static/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='../static/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='../static/favicon-16x16.png' /> */}
+        <meta charSet='utf-8' />
+        <meta name='theme-color' content='#0D0D0D' />
+        <meta property='og:type' content='website' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name='Description' content='YZED: A unique AR experience'></meta>
+        <meta name='description' content='YZED: A unique AR experience'></meta>
+        <meta property='og:url' content='https:/yzed.me' />
+        <meta property='og:title' content='YZED: A unique AR experience' />
+        <meta property='og:description' content='YZED: Check out unique 3D and AR fashion.' />
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://yzed.me' />
+        <meta property='twitter:title' content='YZED: A unique AR experience' />
+        <meta property='twitter:description' content='YZED: Check out unique 3D and AR fashion.' />
         <link
           href='https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700,900&display=swap'
           rel='stylesheet'
           prefetch='true'
         />
+        <link rel='manifest' href='../static/site.webmanifest' />
+        {/* <link rel='manifest' href='/static/manifest.json' /> */}
       </Head>
       <FirebaseProvider>
         <Component {...pageProps} />
